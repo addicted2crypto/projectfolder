13624085307b1e9b0000df229f5fc51c8af14d89
William <101371762+addicted2crypto@users.noreply.github.com>
2023-07-22 20:41:44 -0700

    created todo app
    daily code challenge on 07/22/2023 created crud todo app

---
+++ todo_next.tsx
@@ -0,0 +1,33 @@
+
+import { prisma } from './db'
+
+import Link from "next/link"
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+
+export default async function home() {
+ const todos = await getTodos()
+
+  return (
+    <>
+  <header className="flex justify-between items-center mb-4">
+    <h1 className="text-2xl">Todo List</h1>
+  <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"href="/new">
+   
+   New
+  </Link>
+  </header>
+  <ul className="pl-4">
+    {todos.map(todo => (
+      <TodoItem key={todo.id} {...todo} />
+    ))}
+  </ul>
+  
+
+  </>
+  )
+}
+
