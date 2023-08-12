e6dcac99907155f7f0bccd0e1d6014547fb6d897
William <101371762+addicted2crypto@users.noreply.github.com>
2023-08-11 21:11:22 -0700

    created the start of my crypto app code challenge started on 08/11/2023
    daily code challenge 08/11/2023

---
+++ cryptoinformationapp.tsx
@@ -0,0 +1,32 @@
+
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata = {
+  title: 'That web3 Dapp',
+  description: 'A community curated information funnel!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        
+        <main>
+
+          {children}
+        
+        
+        </main>
+        <Footer />
+        </body>
+
+        
+    </html>
+  )
+}
